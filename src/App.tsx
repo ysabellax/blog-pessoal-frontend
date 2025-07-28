9b4b727bbc1749762f8686843b2c75d998d9f04a
ysabellax <ysabella3909@gmail.com>
2025-07-28 16:45:30 -0300

    Utilizando Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Home from "./pages/home/home"
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
-    return (
-        <Home />
-    )
+  return (
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+
+function Footer() {
+
+    const data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Blog Pessoal Generation | Copyright: {data}
+                        </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
+                    <div className='flex gap-2'>
+                        <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/seu_usuario" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/seu_usuario" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
