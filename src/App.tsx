e03e2cc12a581bb0adc8dfef7e726e521ab68aa1
ysabellax <ysabella3909@gmail.com>
2025-07-30 13:08:07 -0300

    Iniciando Componentes de Temas: Estilização e Lógica

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import FormTema from './components/temas/formtema/FormTema'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import { AuthProvider } from './contexts/AuthContext'
 import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import { AuthProvider } from './contexts/AuthContext'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
 
 function App() {
   return (
@@ -19,6 +22,10 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import axios from "axios"
 
 const api = axios.create({
@@ -12,4 +13,23 @@ export const cadastrarUsuario = async (url: string, dados: object, setDados: Fun
 export const login = async (url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
+}
+
+export const buscar = async (url: string, setDados: Function, header: object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const cadastrar = async (url: string, dados: object, setDados: Function, header: object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: string, dados: object, setDados: Function, header: object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deletar = async (url: string, header: object) => {
+    await api.delete(url, header)
 }
